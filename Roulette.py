a7f04408bcf56dc044ccc1326a87c386c6bddb8e
Catalin Dobrea <dobrea.cat@gmail.com>
2017-01-22 13:20:02 +0100

    După niște dansuri intensive cu bubna, am ajuns la asa hrenoteni

--- Roulette.py
+++ Roulette.py
@@ -1,1 +1,44 @@
-print("Hello Project")
+import random
+
+random.seed(3456)
+
+
+class Roulette:
+    def __init__(self, min):
+        self.min = min
+
+    def simulategame(self, bets, amount):
+
+        def abovemin(amount):
+            above = []
+            for item in amount:
+                above.append(bool(item >= self.min))
+            return above
+
+        def spinthewheel(bets):
+            occ = random.randint(0, 36)
+            print('Spinning the wheel...')
+            print('The Ball lands on...', occ)
+            correcte = []
+            for item in bets:
+                correcte.append(bool(item == occ))
+            if correcte.count(True) == 0:
+                print("No winners this round")
+            else:
+                print ("There are", sum(correcte), "correct bets")
+            return correcte
+
+
+
+
+        y = abovemin(amount)
+        z = spinthewheel(bets)
+        qualify = [i*j for i,j in zip(y,z)]
+        if qualify.count(1) == 0:
+            casinogain = sum(amount)
+            playergains = [0 for i in amount]
+            return [casinogain, playergains]
+        else:
+            playergains = [i*j for i,j in zip(amount, qualify)]
+            casinogain = sum(amount) - sum(playergains)
+            return [casinogain, [i*30 for i in playergains]]
