8541e8cdbbd096fcfbfe13d699a3e304d75ccd4c
Catalin Dobrea <dobrea.cat@gmail.com>
2017-01-24 10:17:48 +0100

    simulation for 10 %

--- Craps.py
+++ Craps.py
@@ -1,6 +1,5 @@
 import random
 # This is used to fixethe random generator so we can test the output
-random.seed(3456)
 
 
 class Craps:
@@ -28,10 +27,15 @@ class Craps:
                 print("No player won")
             return output
 
-        Coeff = [42.65, 35.54, 28.43, 21.32, 14.21, 7.11, 14.21, 21.23, 28.43, 35.54, 42.65]
+        probas = [0.0278, 0.0556, 0.0833, 0.1111, 0.1389, 0.1667, 0.1389, 0.1111, 0.0833, 0.0556, 0.0278]
+        x = [0.9 /j for j in probas]
+        Coeff = [30.490974729241877, 14.216216216216218, 8.804321728691477, 6.100810081008101, 4.484149855907781,
+                 3.4021608643457384, 4.484149855907781, 6.100810081008101, 8.804321728691477, 14.216216216216218,
+                 30.490974729241877]
+
         a = AboveMinimum(amount)
         r = RollTheDice(bet)
         qualify = [i * j for i, j in zip(a, r)]
-        playergains = [i * Coeff[k-2] * j for i, k, j in zip(amount, bet, qualify)]
+        playergains = [i * x[k-2] * j for i, k, j in zip(amount, bet, qualify)]
         casinogain = sum(amount) - sum(playergains)
         return [casinogain, playergains]
