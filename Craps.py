683fdaf91b90e6d4cc22c6cbb70c72630e73f2de
Catalin Dobrea <dobrea.cat@gmail.com>
2017-01-24 11:22:59 +0100

    Clean Craps description + pep8 + proper multiplicatory coefficient explanation

--- Craps.py
+++ Craps.py
@@ -1,5 +1,11 @@
 import random
-# This is used to fixethe random generator so we can test the output
+
+# These are the winning coefficients for every sum of the two dice face. The coefficinets are inversely proportional
+# with probabilities and are computed in such a way that on average the casino gets 10% profit
+
+probas = [0.0278, 0.0556, 0.0833, 0.1111, 0.1389, 0.1667, 0.1389, 0.1111, 0.0833, 0.0556, 0.0278]
+Coeff = [0.9 / j for j in probas]
+
 
 class Craps:
     def __init__(self, min):
@@ -14,20 +20,17 @@ class Craps:
             return output
 
         def RollTheDice(bet):
-            Dices = random.randint(1, 6) + random.randint(1, 6)
+            dice = random.randint(1, 6) + random.randint(1, 6)
             output = []
             for item in bet:
-                output.append(bool(item == Dices))
+                output.append(bool(item == dice))
             print(" Throwing the dice")
-            print(" The sum of the upper faces  ", Dices)
+            print(" The sum of the upper faces  ", dice)
             if sum(output) > 0:
                 print(" There are ", sum(output), " winner(s)")
             else:
                 print("No player won")
             return output
-
-        probas = [0.0278, 0.0556, 0.0833, 0.1111, 0.1389, 0.1667, 0.1389, 0.1111, 0.0833, 0.0556, 0.0278]
-        Coeff = [0.9 /j for j in probas]
 
         a = AboveMinimum(amount)
         r = RollTheDice(bet)
