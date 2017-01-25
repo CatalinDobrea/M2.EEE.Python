6bf8db6aa30e0e3377611c1c7745a3069282cabf
Catalin Dobrea <dobrea.cat@gmail.com>
2017-01-25 16:24:04 +0100

    Added the function which assingns custommers to tables.

--- Evening.py
+++ Evening.py
@@ -1,111 +1,62 @@
 import random
-random.seed(3456)
+import Roulette
+import Craps
 
-# EXCERCISE 5
 class Customer(object):
-    def __init__(self, typeC):
+    def __init__(self, typeC, ID):
         self.typeC = typeC
-        if self.typeC == "Returning":
+        self.ID = ID
+        if self.typeC == "Returning" :
             self.bet = 10
             self.budget = random.randint(100,300)
-        if self.typeC == "Onetime":
+        if self.typeC == "Onetime" :
             self.budget = random.randint(200,300)
             self.bet = random.randint(0,int((self.budget)/3))
         if self.typeC == "Bachelor":
             self.budget = random.randint(200,500)
             self.bet = random.randint(0,int(self.budget))
 
+
 def CustomerType(returning, bachelor, total, startcash):
     customers = []
     ret = int(total * returning / 100)
     bch = int(total * bachelor / 100)
-    onet = total - ret -bch
+    onet = total - ret - bch
     for i in range(ret):
-        customers.append(Customer('Returning'))
+        customers.append(Customer('Returning',i))
     for i in range(onet):
-        customers.append(Customer('Onetime'))
+        customers.append(Customer('Onetime', i+ret))
     for i in range(bch):
-        customers.append(Customer('Bachelor'))
+        customers.append(Customer('Bachelor', i+onet+ret))
     for i in range(100-bch, total):
         customers[i].budget += startcash
     return customers
 
 customers = list(CustomerType(50, 10, 100, 200))
-for i in customers:
-    print(i.budget)
+#for i in customers:
+#    print(i.ID)
+
+
+# class Table(object):
+#     def __init__(self, IDT, typeT, players):
+#         self.typeT = typeT
+#         self.players = players
+#         self.IDT = IDT
+
+print(len(customers))
 
 
 
+def repartition(tbnum):
+    tables = [[] for f in range(tbnum)]
+    aux = customers
+    for i in range(tbnum):
+        if aux != []:
+            tables[i] = random.sample(aux, random.randint(1, len(aux)))
+            aux = [x for x in aux if x not in tables[i]]
+    return tables
+
+print(repartition(20))
 
 
-
-
-
-# for i in range(100):
-#     customers.append(Customer('Returning'))
-# for obj in customers:
-#     print(obj.budget)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''    def averagegrade(self):
-        sum = 0
-        for student in self.studentslist:
-            sum += student.grade
-        return float(sum/len(self.studentslist))
-
-    def givegrades(self):
-        return False
-
-class Workshop(Class):
-
-    def givegrades(self):
-
-        for student in self.studentslist:
-            student.gradeStudent(self.teacher)
-
-        for index, student in enumerate(self.studentslist):
-            if index %2 ==0:
-                if index+1 < len(self.studentslist):
-                    student.grade = (student.grade + self.studentslist[index+1].grade)/2
-            else:
-                student.grade = self.studentslist[index-1].grade
-
-class Lecture(Class):
-
-    def givegrades(self):
-
-        for student in self.studentslist:
-            student.gradeStudent(self.teacher)
-
-from random import randint
-
-class Teacher(object):
-
-
-    def __init__(self, strictness):
-        self.strictness = strictness
-
-
-class Student(object):
-    def __init__(self, intelect):
-        self.intelect = intelect
-    def gradeStudent(self, teacher):
-        self.grade  = self.intelect * (1-teacher.strictness) * float(randint(5,10)/10.0)
-
-class ErasmusStudent(Student):
-    def gradeStudent(self, teacher):
-           self.grade  = self.intelect * (1-teacher.strictness*0.8) * float(randint(5,10)/10.0) '''
+vafli  = random.sample(customers,random.randint(1,len(customers)))
